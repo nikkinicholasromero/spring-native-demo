1bffc947bab2fe1e9064829a85dcb2660597ac73
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2022-08-01 05:02:32 +0800

    Initial commit

---
+++ src/main/java/com/demo/Application.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+import com.sun.tools.javac.Main;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import reactor.netty.DisposableChannel;
+import reactor.netty.http.server.HttpServer;
+
+public class Application {
+    public static void main(String[] args) {
+        HttpHandler httpHandler = RouterFunctions.toHttpHandler(
+                new MainRouter().route()
+        );
+
+        ReactorHttpHandlerAdapter reactorHttpHandler = new ReactorHttpHandlerAdapter(httpHandler);
+
+        HttpServer.create()
+                .port(8080)
+                .handle(reactorHttpHandler)
+                .bind()
+                .flatMap(DisposableChannel::onDispose)
+                .block();
+    }
+}
